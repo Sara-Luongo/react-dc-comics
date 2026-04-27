57d738898512b92e250dcd02da8e835ecd9c37ee
sara luongo <saraluongo202002@gmail.com>
2026-04-27 17:11:17 +0200

    aggiunto main con sezione contenuto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./componenti/Header.jsx"
-
+import Main from "./componenti/Main.jsx"
 
 function App() {
 
-  return (
+  return <>
     <Header />
-  )
+    <Main />
+  </>
 }
 
 export default App
---
+++ src/componenti/Main.jsx
@@ -0,0 +1,10 @@
+function Main() {
+    return (
+        <main>
+            <section>
+                <h1>content goes here</h1>
+            </section>
+        </main>
+    )
+}
+export default Main;
