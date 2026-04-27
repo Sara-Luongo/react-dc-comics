78f0d24cc4556d5073488342e9b4e02de0258d9a
sara luongo <saraluongo202002@gmail.com>
2026-04-27 17:53:26 +0200

    creata struttura main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Header from "./componenti/Header.jsx"
 import Main from "./componenti/Main.jsx"
+import Footer from "./componenti/Footer.jsx"
+
 
 function App() {
 
   return <>
     <Header />
     <Main />
+    <Footer />
   </>
 }
 
---
+++ src/componenti/Footer.jsx
@@ -0,0 +1,76 @@
+function Footer() {
+    return <>
+        <footer>
+            <section>
+                <div>
+                    <ul>
+                        <li><img src="/img/buy-comics-digital-comics.png" alt="" />DIGITAL COMICS</li>
+                        <li><img src="/img/buy-comics-merchandise.png" alt="" />DC MERCHANDISE</li>
+                        <li><img src="/img/buy-comics-shop-locator.png" alt="" />SUBSCRIPTION</li>
+                        <li><img src="/img/buy-comics-subscriptions.png" alt="" />COMIC SHOP LOCATOR</li>
+                        <li><img src="/img/buy-dc-power-visa.svg" alt="" />DC POWER VISA</li>
+                    </ul>
+                </div>
+            </section>
+            <section>
+                <div>
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <h3>DC</h3>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <h3>SITIES</h3>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <h3>SHOP</h3>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </section>
+            <div>
+                <button>SING-UP NOW!</button>
+            </div>
+            <div>
+                <h3>FOLLOW US</h3>
+                <ul>
+                    <li><img src="public/img/footer-facebook.png" alt="" /></li>
+                    <li><img src="public/img/footer-periscope.png" alt="" /></li>
+                    <li><img src="public/img/footer-pinterest.png" alt="" /></li>
+                    <li><img src="public/img/footer-twitter.png" alt="" /></li>
+                    <li><img src="public/img/footer-youtube.png" alt="" /></li>
+                </ul>
+            </div>
+            <section>
+
+            </section>
+        </footer>
+    </>
+
+}
+export default Footer;
