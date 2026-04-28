edf8d5117f2977e4efa1ccfb96fe3ae567d78a89
sara luongo <saraluongo202002@gmail.com>
2026-04-28 16:46:08 +0200

    modificato array in header in arrai di oggetti con title e id

--- src/componenti/Header.jsx
+++ src/componenti/Header.jsx
@@ -1,26 +1,65 @@
 import './Header.css'
 
-const navMenuItems = ['CHARACTERS', 'COMICS', 'MOVIES', 'TV', 'GAMES', 'COLLECTIBLES', 'VIDEOS', 'FANS', 'NEWS', 'SHOP']
+const navMenuItems = [
+    {
+        title: 'COMICS',
+        id: 1
+    },
+    {
+        title: 'MOVIES',
+        id: 2
+    },
+    {
+        title: 'TV',
+        id: 3
+    },
+    {
+        title: 'GAMES',
+        id: 4
+    },
+    {
+        title: 'COLLECTIBLES',
+        id: 5
+    },
+    {
+        title: 'VIDEOS',
+        id: 6
+    },
+    {
+        title: 'FANS',
+        id: 7
+    },
+    {
+        title: 'NEWS',
+        id: 8
+    },
+    {
+        title: 'SHOP',
+        id: 9
+    }
+]
 
-const menuItems = navMenuItems.map((items, index) => {
-    return (<li key={index}>
-        <a
-            href="#">{items}
-        </a>
+const menuItems = navMenuItems.map((items) => {
+    const { title, id } = items
+    return (<li key={id}>
+        {title}
     </li>)
 })
 
 function Header() {
     return (
-        <header className='header' >
+        <header className='header'>
             <nav className='header-navbar'>
-                <a href='#'>
-                    <img src="/img/dc-logo.png" alt="logo della DC" />
-                </a>
-
-                <ul className='header-list'>
-                    {menuItems}
-                </ul>
+                <div>
+                    <a href='#'>
+                        <img src="/img/dc-logo.png" alt="logo della DC" />
+                    </a>
+                </div>
+                <div>
+                    <ul className='header-list'>
+                        {menuItems}
+                    </ul>
+                </div>
             </nav>
         </header>
     )
